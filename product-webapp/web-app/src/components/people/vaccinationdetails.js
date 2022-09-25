ae9761b4b1d9885f4fcca101a7046532b36cae01
obulreddy <obul0102@gmail.com>
2022-09-25 13:52:28 +0530

    ip

--- product-webapp/web-app/src/components/people/vaccinationdetails.js
+++ product-webapp/web-app/src/components/people/vaccinationdetails.js
@@ -11,7 +11,7 @@ function Vaccinationdetails() {
 
     var emailPass = localStorage.getItem("userEmailId");
     const getVCDetails = () => {
-        return fetch("http://3.90.253.77:8080/user-service/api/v1/user/alluser/" + emailPass, {
+        return fetch("http://3.19.237.179 :8080/user-service/api/v1/user/alluser/" + emailPass, {
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json'
